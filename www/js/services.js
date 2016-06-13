66da0915f0b5a4b8e2e2f09f976c6bbadcb7e10f
Ruben Laube-Pohto <ruben.laubepohto@gmail.com>
2016-06-13 10:16:49 +0300

    Add basic BaasBox service

--- www/js/services.js
+++ www/js/services.js
@@ -47,4 +47,50 @@ angular.module('starter.services', [])
       return null;
     }
   };
+})
+
+.service('BaasBoxService', function($http) {
+  var baseUrl = "http://192.168.142.37:9000";
+  var appcode = "1234567890";
+  var token = "";
+
+  this.login = function(username, password) {
+    $http.post(baseUrl + "/login", {"username": username, "password": password, "appcode": appcode})
+      .then(function (body) {
+        token = body.data.data["X-BB-SESSION"];
+        console.log(token);
+      }, function(error) {
+        console.log(error);
+      });
+  };
+
+  // Get all devices from database
+  this.getDevices = function() {
+    if (token.length > 0) {
+      $http.get(baseUrl + "/document/raspit", {headers: {"X-BB-SESSION": token}})
+        .then(function(body) {
+          console.log(body);
+        }, function(error) {
+          console.log(error);
+        });
+    }
+    else {
+      console.log("Not signed in.")
+    }
+  };
+
+  // Add new device to the database
+  this.addDevice = function(device) {
+    // Unchecked checkbox values are not passed forward from form. Add false.
+    if (!device.status) {
+      device.status = false;
+    }
+    console.log(device);
+    $http.post(baseUrl + "/plugin/new_device.script", device, {headers: {"X-BB-SESSION": token}})
+      .then(function(body) {
+        console.log(body);
+      }, function(error) {
+        console.log(error);
+      });
+  };
 });
