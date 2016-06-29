a7ffe22017fb488b5cb5c2cfbfc6f59622b8a129
Ruben Laube-Pohto <h8871@student.jamk.fi>
2016-06-29 10:49:32 +0300

    Remove getting tasks form login controller

--- www/js/login/login.controller.js
+++ www/js/login/login.controller.js
@@ -18,12 +18,14 @@ function LoginController ($ionicPopup, $scope, $state, BaasBoxService, ErrorServ
         token = body.data.data["X-BB-SESSION"];
         // Save token for further calls that require authentication
         BaasBoxService.setToken(token)
+        /*
         BaasBoxService.getTasks()
           .then(function (body) {
             TasksService.set(body.data.data)
           }, function (error) {
             ErrorService.handleError(error)
           })
+          */
         $state.go('tab.tasks')
       }, function(error) {
         ErrorService.handleError(error)
