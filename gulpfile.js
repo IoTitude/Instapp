900cad698668ec1113345dbf412553062a1a8f3c
Ruben Laube-Pohto <h8871@student.jamk.fi>
2016-06-30 09:52:25 +0300

    Wrap all app js into one file
    Wrapped all of the application's javascript files into one file. The file is just a concatenated version of all the different files. It would be good to have the file minified but I don't know how to do that. I tried to use gulp-uglify but it broke the code.

--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,11 @@ var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var sh = require('shelljs');
+var uglify = require('gulp-uglify');
 
 var paths = {
-  sass: ['./scss/**/*.scss']
+  sass: ['./scss/**/*.scss'],
+  js: ['./www/js/**/*.js']
 };
 
 gulp.task('default', ['sass']);
@@ -28,6 +30,7 @@ gulp.task('sass', function(done) {
 
 gulp.task('watch', function() {
   gulp.watch(paths.sass, ['sass']);
+  gulp.watch(paths.js, ['minjs']);
 });
 
 gulp.task('install', ['git-check'], function() {
@@ -48,4 +51,29 @@ gulp.task('git-check', function(done) {
     process.exit(1);
   }
   done();
+});
+
+gulp.task('minjs', function (done) {
+  gulp.src(
+    [
+      './www/js/app.js',
+      './www/js/appconfig/routes.js',
+      './www/js/baasbox/baasbox.service.js',
+      './www/js/baasbox/baasbox.config.js',
+      './www/js/error/error.service.js',
+      './www/js/error/error.config.js',
+      './www/js/login/login.controller.js',
+      './www/js/logout/logout.controller.js',
+      './www/js/tasks/detail.controller.js',
+      './www/js/tasks/tasks.controller.js',
+      './www/js/tasks/tasks.service.js'
+    ])
+    .pipe(concat('app.bundle.js'))
+    .pipe(gulp.dest('./www/js/'))
+    // Uglifying the bundled app doesn't work with this configuration.
+    // Somehow the dependency injections break.
+    //.pipe(rename('app.bundle.min.js'))
+    //.pipe(uglify())
+    //.pipe(gulp.dest('./www/js/'))
+    .on('end', done);
 });
---
+++ www/js/app.bundle.js
@@ -0,0 +1,354 @@
+/*
+ * Main module for Instapp.
+ *
+ * Combines all required modules together and starts the application.
+ */
+angular
+  .module('instapp', [
+    'ionic',
+    'instapp.detailController',
+    'instapp.loginController',
+    'instapp.logoutController',
+    'instapp.tasksController',
+    'instapp.baasBoxService',
+    'instapp.errorService'])
+  .run(appRun)
+
+function appRun ($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+}
+
+angular
+  .module('instapp')
+  .config(config)
+
+function config ($stateProvider, $urlRouterProvider) {
+  $stateProvider
+  // setup an abstract state for the tabs directive
+    .state('tab', {
+      url: '/tab',
+      abstract: true,
+      templateUrl: 'templates/tabs.html'
+    })
+
+  // Each tab has its own nav history stack:
+
+    .state('tab.tasks', {
+      url: '/tasks',
+      views: {
+        'tab-tasks': {
+          templateUrl: 'templates/tab-tasks.html',
+          controller: 'TasksController'
+        }
+      }
+    })
+
+    .state('tab.task-detail', {
+      url: '/tasks/:taskName',
+      views: {
+        'tab-tasks': {
+          templateUrl: 'templates/task-detail.html',
+          controller: 'DetailController'
+        }
+      }
+    })
+
+    .state('tab.qr', {
+        url: '/qr',
+        views: {
+          'tab-qr': {
+            templateUrl: 'templates/tab-qr.html'
+          }
+        }
+    })
+
+    .state('tab.logout', {
+      url: '/logout',
+      views: {
+        'tab-logout': {
+          templateUrl: 'templates/tab-logout.html',
+          controller: 'LogoutController'
+        }
+      }
+    })
+
+    .state('login', {
+      url: '/login',
+      templateUrl: 'templates/login.html',
+      controller: 'LoginController'
+    })
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/login')
+}
+
+// Service for communicating with the BaasBox API
+
+angular
+  .module('instapp.baasBoxService', [])
+  .service('BaasBoxService', BaasBoxService)
+
+  BaasBoxService.$inject = ['$http', 'SERVER_CONFIG']
+
+  function BaasBoxService ($http, SERVER_CONFIG) {
+    var baseUrl = SERVER_CONFIG.BASE_URL
+    var appcode = SERVER_CONFIG.APPCODE
+
+    // Get all headers required by different calls.
+    //
+    // Headers that are not required by a call are just discarded by the
+    // BaasBox backend.
+    var getHeaders = function () {
+      return {
+        headers: {
+          "X-BB-SESSION": localStorage.getItem('BaasBoxToken'),
+          "X-BAASBOX-APPCODE": appcode,
+          "Content-type": "application/json"}
+      }
+    }
+
+    this.login = function(username, password) {
+      var url = baseUrl + "/login"
+      return $http.post(url, {"username": username, "password": password, "appcode": appcode})
+    }
+
+    // Get all tasks from database
+    this.getTasks = function() {
+      var url = baseUrl + "/document/Master"
+      return $http.get(url, getHeaders())
+    }
+
+    // Logout
+    this.logout = function () {
+      var url = baseUrl + "/logout"
+      return $http.post(url, {}, getHeaders())
+    }
+
+    // Update task status through BaasBox REST API
+    this.toggleTask = function (task) {
+      var id = task.id
+      var url = baseUrl + "/document/Master/" + id + "/.enabled"
+      var body = {
+        "data": task.enabled
+      }
+      return $http.put(url, body, getHeaders())
+    }
+
+    // Update task status through BaasBox plugin
+    this.toggleTaskSDN = function (task) {
+      var url = baseUrl + "/plugin/instapp.toggle"
+      var body = task
+      return $http.put(url, body, getHeaders())
+    }
+
+    // Set token
+    this.setToken = function (newToken) {
+      localStorage.setItem('BaasBoxToken', newToken)
+    }
+  }
+
+/* Server configuration
+ *
+ * This information might be best hidden from GitHub but at this point in
+ * development it doesn't matter much.
+ */
+angular
+  .module('instapp.baasBoxService')
+  .constant('SERVER_CONFIG', {
+    'BASE_URL': 'http://82.196.14.4:9000',
+    'APPCODE': '1234567890'
+  })
+
+angular
+  .module('instapp.errorService', [])
+  .service('ErrorService', ErrorService)
+
+ErrorService.$inject = ['errMsg', '$ionicPopup']
+
+function ErrorService (errMsg, $ionicPopup) {
+  this.handleError = function (error) {
+    var status = error.status
+
+    switch (status) {
+      case -1: {
+        $ionicPopup.alert({
+          title: errMsg.SERVER_NOT_FOUND
+        })
+      } break
+      case 400: {
+        $ionicPopup.alert({
+          title: errMsg.BAD_REQUEST
+        })
+      } break
+      case 401: {
+        $ionicPopup.alert({
+          title: errMsg.UNAUTHORIZED
+        })
+      } break
+      default: {
+        $ionicPopup.alert({
+          title: errMsg.DEFAULT
+        })
+      }
+    }
+  }
+}
+
+angular
+  .module('instapp.errorService')
+  .constant('errMsg', {
+    DEFAULT: 'Something went wrong. Sorry :(',
+    SERVER_NOT_FOUND: 'Unable to connect to server.',
+    BAD_REQUEST: '400: Check your username and password.',
+    UNAUTHORIZED: '401: Unathorized. Please sign in first.'
+  })
+
+angular
+  .module('instapp.loginController', [])
+  .controller('LoginController', LoginController)
+
+LoginController.$inject = [
+  '$ionicPopup',
+  '$scope',
+  '$state',
+  'BaasBoxService',
+  'ErrorService',
+  'TasksService']
+
+function LoginController ($ionicPopup, $scope, $state, BaasBoxService, ErrorService, TasksService) {
+  $scope.data = {}
+  $scope.login = function() {
+    BaasBoxService.login($scope.data.username, $scope.data.password)
+      .then(function (body) {
+        token = body.data.data["X-BB-SESSION"];
+        // Save token for further calls that require authentication
+        BaasBoxService.setToken(token)
+        $state.go('tab.tasks')
+      }, function(error) {
+        ErrorService.handleError(error)
+      })
+  }
+}
+
+angular
+  .module('instapp.logoutController', [])
+  .controller('LogoutController', LogoutController)
+
+LogoutController.$inject = [
+  '$ionicPopup',
+  '$scope',
+  '$state',
+  'BaasBoxService',
+  'ErrorService']
+
+function LogoutController ($ionicPopup, $scope, $state, BaasBoxService, ErrorService) {
+  $scope.logout = function () {
+    BaasBoxService.logout()
+      .then(function (body) {
+        localStorage.removeItem('BaasBoxToken')
+        $state.go('login')
+      }, function (error) {
+        ErrorService.handleError(error)
+      })
+  }
+}
+
+angular
+  .module('instapp.detailController', ['instapp.tasksService'])
+  .controller('DetailController', DetailController)
+
+DetailController.$inject = [
+  '$ionicPopup',
+  '$scope',
+  '$stateParams',
+  'BaasBoxService',
+  'ErrorService',
+  'TasksService']
+
+function DetailController ($ionicPopup, $scope, $stateParams,
+  BaasBoxService, ErrorService, TasksService) {
+    $scope.$on('$ionicView.beforeEnter', function () {
+      // parameter name 'taskName' must match the parameter defined in the state url
+      $scope.task = TasksService.getTask($stateParams.taskName)
+    })
+
+    $scope.toggle = function (task) {
+      BaasBoxService.toggleTaskSDN(task)
+        .then(function (body) {
+          $ionicPopup.alert({
+            title: body.data.result
+          })
+        }, function (error) {
+          ErrorService.handleError(error)
+        })
+    }
+}
+
+angular
+  .module('instapp.tasksController', ['instapp.tasksService'])
+  .controller('TasksController', TasksController)
+
+TasksController.$inject = [
+  '$scope',
+  '$ionicPopup',
+  'BaasBoxService',
+  'ErrorService',
+  'TasksService']
+
+function TasksController ($scope, $ionicPopup, BaasBoxService, ErrorService, TasksService) {
+  $scope.tasks = TasksService.get()
+
+  $scope.$on("$ionicView.beforeEnter", function(event, data){
+     $scope.update()
+  })
+
+  $scope.update = function () {
+    BaasBoxService.getTasks()
+      .then(function (body) {
+        $scope.tasks = body.data.data
+        TasksService.set($scope.tasks)
+      }, function (error) {
+        ErrorService.handleError(error)
+      })
+      .finally(function () {
+        // Stop the ion-refresher from spinning
+       $scope.$broadcast('scroll.refreshComplete')
+      })
+  }
+}
+
+angular
+  .module('instapp.tasksService', [])
+  .factory('TasksService', TasksService)
+
+function TasksService () {
+  var tasks = {}
+
+  return {
+    set: function (t) {
+      tasks = t
+    },
+    get: function () {
+      return tasks
+    },
+    getTask: function (id) {
+      for (i = 0; i < tasks.length; i++) {
+        if (tasks[i].mac === id) {
+          return tasks[i]
+        }
+      }
+    }
+  }
+}
