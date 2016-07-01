72d5255c4d1eb657a4c99d9896117046a70871d5
Ruben Laube-Pohto <h8871@student.jamk.fi>
2016-07-01 11:28:24 +0300

    Comment tasks controller module

--- www/js/tasks/tasks.controller.js
+++ www/js/tasks/tasks.controller.js
@@ -1,3 +1,9 @@
+/*
+ * Task controller module
+ *
+ * Controlls the behavior of the task list view.
+ */
+
 angular
   .module('instapp.tasksController', ['instapp.tasksService'])
   .controller('TasksController', TasksController)
@@ -12,10 +18,12 @@ TasksController.$inject = [
 function TasksController ($scope, $ionicPopup, BaasBoxService, ErrorService, TasksService) {
   $scope.tasks = TasksService.get()
 
+  // Get list of tasks on opening the tab
   $scope.$on("$ionicView.beforeEnter", function(event, data){
      $scope.update()
   })
 
+  // Update task list
   $scope.update = function () {
     BaasBoxService.getTasks()
       .then(function (body) {
