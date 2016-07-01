00d6b054afe9ae05f781fe363dd7557501cc0948
Ruben Laube-Pohto <h8871@student.jamk.fi>
2016-07-01 11:32:35 +0300

    Comment tasks service module

--- www/js/tasks/tasks.service.js
+++ www/js/tasks/tasks.service.js
@@ -1,3 +1,11 @@
+/*
+ * Tasks service module
+ *
+ * This is a helper service that is used for sharing a common list of
+ * tasks between different modules and their controllers. Angular services
+ * can store information while the app is running.
+ */
+
 angular
   .module('instapp.tasksService', [])
   .factory('TasksService', TasksService)
@@ -6,12 +14,15 @@ function TasksService () {
   var tasks = {}
 
   return {
+    // Set tasks list
     set: function (t) {
       tasks = t
     },
+    // Get tasks list
     get: function () {
       return tasks
     },
+    // Get a task from the tasks list. Mac address is used as a unique id.
     getTask: function (id) {
       for (i = 0; i < tasks.length; i++) {
         if (tasks[i].mac === id) {
